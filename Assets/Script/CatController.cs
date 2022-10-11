51482be8e8ffc723bb7c9bf0d471acb83fd0edda
Aeon-Domini <111920923+Aeon-Domini@users.noreply.github.com>
2022-10-11 13:08:07 -0400

    animation commit

--- Assets/Script/CatController.cs
+++ Assets/Script/CatController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewScript : MonoBehaviour
+public class CatController : MonoBehaviour
 {
 
     public AudioClip musicClipOne;
@@ -11,34 +11,78 @@ public class NewScript : MonoBehaviour
 
     public AudioSource musicSource;
 
+    Animator anim;
+
+void Start()
+
+{
+
+  anim = GetComponent<Animator>();
+
+}
+
 // Update is called once per frame
 void Update()
 {
      if (Input.GetKeyDown(KeyCode.W))
+
         {
+
           musicSource.clip = musicClipOne;
+
           musicSource.Play();
+
+          anim.SetInteger("State", 1);
 
          }
 
      if (Input.GetKeyUp(KeyCode.W))
+
         {
+
           musicSource.Stop();
+
+          anim.SetInteger("State", 0);
 
          }
 
      if (Input.GetKeyDown(KeyCode.R))
+
         {
+
           musicSource.clip = musicClipTwo;
+
           musicSource.Play();
+
+          anim.SetInteger("State", 2);
+
          }
 
      if (Input.GetKeyUp(KeyCode.R))
+
         {
+
           musicSource.Stop();
+
+          anim.SetInteger("State", 0);
 
          }
 
+     if (Input.GetKeyDown(KeyCode.L))
+
+        {
+
+          musicSource.loop = true;
+
+         }
+
+     if (Input.GetKeyUp(KeyCode.L))
+
+        {
+
+          musicSource.loop = false;
+
+        }
 
    }
 }
